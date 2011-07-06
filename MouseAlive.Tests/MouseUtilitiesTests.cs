66df89356a343065161973c0833cd5b135df209a
Robert Greiner <robert@robertgreiner.com>
2011-07-06 13:06:36 -0500

    Added MouseUtilities with NUnit tests

---
+++ MouseAlive.Tests/MouseUtilitiesTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Drawing;
+
+namespace MouseAlive.Tests {
+
+    [TestFixture]
+    class MouseUtilitiesTests {
+
+        [SetUp]
+        public void SetUp() {
+
+        }
+
+        [Test]
+        public void CurrentMousePositionIsNotNull() {
+            var point = MouseUtilities.GetMousePosition();
+            Assert.IsNotNull(point);
+        }
+
+        [Test]
+        public void CurrentMousePositionCoordinatesAreNotNegative() {
+            var point = MouseUtilities.GetMousePosition();
+            Assert.IsTrue(point.X > 0);
+            Assert.IsTrue(point.Y > 0);
+        }
+
+        [Test]
+        public void NewMousePositionDoesNotEqualOldMousePositionAfterMouseMove() {
+            var oldPoint = MouseUtilities.GetMousePosition();
+            MouseUtilities.MoveMouse();
+            var newPoint = MouseUtilities.GetMousePosition();
+            Assert.AreNotEqual(oldPoint, newPoint);
+        }
+
+        [Test]
+        public void NewMouseXPositionIsOneGreaterThanOldMouseXPosition() {
+            var oldPoint = MouseUtilities.GetMousePosition();
+            MouseUtilities.MoveMouse();
+            var newPoint = MouseUtilities.GetMousePosition();
+            Assert.AreEqual(oldPoint.X + 1, newPoint.X);
+        }
+
+        [Test]
+        public void NewMouseYPositionIsEqualToTheOldMouseYPosition() {
+            var oldPoint = MouseUtilities.GetMousePosition();
+            MouseUtilities.MoveMouse();
+            var newPoint = MouseUtilities.GetMousePosition();
+            Assert.AreEqual(oldPoint.Y, newPoint.Y);
+        }
+
+        [Test]
+        public void NewMouseCoordinatesAreEqualToTheOldMouseCoordinatesOnMoveBack() {
+            var oldPoint = MouseUtilities.GetMousePosition();
+            MouseUtilities.MoveMouse();
+            MouseUtilities.MoveMouseBack();
+            var newPoint = MouseUtilities.GetMousePosition();
+            Assert.AreEqual(oldPoint, newPoint);
+        }
+    }
+}
---
+++ MouseAlive/MouseUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace MouseAlive {
+    public class MouseUtilities {
+        public static System.Drawing.Point GetMousePosition() {
+            return System.Windows.Forms.Control.MousePosition;
+        }
+
+        public static void MoveMouse() {
+            Cursor.Position = new System.Drawing.Point(Cursor.Position.X + 1, Cursor.Position.Y);
+        }
+
+        public static void MoveMouseBack() {
+            Cursor.Position = new System.Drawing.Point(Cursor.Position.X - 1, Cursor.Position.Y);
+        }
+    }
+}
