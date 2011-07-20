c4deb0ccfaf3eea28074d91baa63ed07751e047b
Robert Greiner <robert@robertgreiner.com>
2011-07-19 22:44:21 -0500

    Added Acceptance Tests with SpecFlow

---
+++ MouseAlive.AcceptanceTests/MouseUtilitiesSteps.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace MouseAlive.AcceptanceTests {
+    [Binding]
+    public class MouseUtilitiesSteps {
+        
+        private Point point;
+
+        [Given("I am working on a machine with MouseAlive")]
+        public void GivenIAmWorkingOnAMachineWithMouseAlive() {
+            point = MouseUtilities.GetMousePosition();
+        }
+
+        [When("a certain amount of time has elapsed")]
+        public void WhenIAmAwayFromMyDesk() {
+            var oldPoint = point;
+            MouseUtilities.MoveMouse();
+           
+        }
+
+        [Then("the mouse should move automatically")]
+        public void ThenTheMouseShouldMoveAutomaticallyOverATimeInterval() {
+            var newPoint = MouseUtilities.GetMousePosition();
+            Assert.AreNotEqual(point, newPoint);
+        }
+    }
+}
